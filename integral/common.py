37af4c108cc6f0e1ec2598e060f31604d98e62f5
evgeniiayd <evgeniiappletree@mail.ru>
2023-10-04 11:10:09 +0300

    feat: common.py

--- integral/common.py
+++ integral/common.py
@@ -3,5 +3,18 @@ def math_func_for_example(x: int) -> int:
 
 
 class Integral:
-    def constant_step(self, step: int = 1):
-        return step
+    def constant_step(self):
+        print(f"Введите количество частей N")
+        N = int(input())
+        print(f"Введите нижний предел a")
+        a = int(input())
+        print(f"Введите верхний предел b")
+        b = int(input())
+        h = (b - a) / N
+        s = 0
+        x = a
+        while (x <= (b - h)):
+            s = math_func_for_example(x)
+            x = x + h
+        I = h * s
+        print(f'Ответ: {I}')
