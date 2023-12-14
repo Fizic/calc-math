59c7e78e26fdf99064140516d172cb466a09dcaa
evgeniiayd <evgeniiappletree@mail.ru>
2023-12-15 02:32:20 +0300

    commit all

--- app/calculations/elementary_function/chebyshev.py
+++ app/calculations/elementary_function/chebyshev.py
@@ -1,27 +1,37 @@
-from math import log
-
-
 class ChebyshevSolver:
-    def __init__(self, x: float, func: float):
+    def __init__(self, x: float):
         self.x = x
-        if func == 1:  # exp(x)
-            self.arr = [0.9999998, 1, 0.5000063, 0.1666674, 0.0416350, 0.0083298, 0.0014393, 0.0002040]
-            self.accuracy = 2e-7
-        elif func == 2:  # sin(x)
-            self.arr = [1.000000002, -0.166666589, 0.008333075, -0.000198107, 0.000002608]
-            self.accuracy = 6e-9
 
-    def chebyshev(self):
-        c = self.arr[0]
+    def chebyshev_exp(self):
+        arr = [0.9999998, 1, 0.5000063, 0.1666674, 0.0416350, 0.0083298, 0.0014393, 0.0002040]
+        accuracy = 2e-7
+        c = arr[0]
         p = 1
         k = 1
         u = 1000
-        while abs(u) > self.accuracy:
+        while abs(u) > accuracy:
             p = p * self.x
-            u = p * self.arr[k]
+            u = p * arr[k]
             c = c + u
             k += 1
-            if k == len(self.arr):
+            if k == len(arr):
                 break
-        answer = round(c, abs(int(log(self.accuracy, 10))) + 1)
+        answer = round(c, 3)
+        return answer
+
+    def chebyshev_sin(self):
+        arr = [1.000000002, -0.166666589, 0.008333075, -0.000198107, 0.000002608]
+        accuracy = 6e-9
+        c = 0
+        p = 1/self.x
+        k = 0
+        u = 1000
+        while abs(u) > accuracy:
+            p = p*self.x*self.x
+            u = p * arr[k]
+            c = c + u
+            k += 1
+            if k == len(arr):
+                break
+        answer = round(c, 3)
         return answer
--- app/cmd/elementary_function/chebyshev.py
+++ app/cmd/elementary_function/chebyshev.py
@@ -5,22 +5,25 @@ from app.cmd.base import BaseCmd
 class ChebyshevCmd(BaseCmd):
     """
     Вычисление значения функции в точке по методу Чебышева
-    func = 1 - функция exp(x)
-    func = 2 - функция sin(x)
     """
-    def __init__(self, x: float = None, func: float = None, **kwargs):
+    def __init__(self, x: float = None, **kwargs):
         self.x = x
-        self.func = func
 
         self.type_of_calculations = 'elementary_function_chebyshev'
         self.method = ''
 
     def __start(self):
         super().__init__()
-        self._chebyshev_solver = ChebyshevSolver(self.x, self.func)
+        self._chebyshev_solver = ChebyshevSolver(self.x)
 
-    def __call__(self):
+    def exp(self):
         self.__start()
-        self.method = 'chebyshev'
-        self.answer = self._chebyshev_solver.chebyshev()
+        self.method = 'chebyshev_exp'
+        self.answer = self._chebyshev_solver.chebyshev_exp()
+        self._finish()
+
+    def sin(self):
+        self.__start()
+        self.method = 'chebyshev_sin'
+        self.answer = self._chebyshev_solver.chebyshev_sin()
         self._finish()
